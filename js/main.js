45a1f6053e2a9c32139002afce9ba31905b61c26
William Kategianes <wKategianes@gmail.com>
2023-01-16 09:28:20 -0800

    Updated main.js
    Updated main.css
    Updated main.js
    Updated index.html

--- js/main.js
+++ js/main.js
@@ -18,69 +18,110 @@ let winner;
 
 /*----- cached elements  -----*/
 const messageEl = document.querySelector("h1");
-// board reset
 const resetButton = document.querySelector("button");
-const markerEls = [...document.querySelectorAll("#board > div")];
 
 
 /*----- event listeners -----*/
-document.getElementById("board").addEventListener("click", positionSelect);
-resetButton.addEventListener("click", init)
+document.getElementById("board").addEventListener("click", squareSelect);
+resetButton.addEventListener("click", init);
 
 
 /*----- functions -----*/
 init()
 
 function init() {
-    board = [0,0,0,0,0,0,0,0,0]
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     turn = 1;
     winner = null;
     render();
+    resetButton.style.visibility = "hidden";
 }
 
-function positionSelect (event) {
+function squareSelect (event) {
 
-    const Idx = event.target.id;
-    if (board[Idx] !== 0 || winner) return;
-    board[Idx] = turn;
+    const idx = event.target.id;
+    console.log(idx);
+    if (board[idx] !== 0 || winner) return;
+    board[idx] = turn;
     turn *= -1;
 
-    // winner = getWinner();
+    winner = getWinner();
     render();
 }
+
+function getWinner() {
+    
+    // checking for ties
+    let emptySpaces = 0;
+    for (let i = 0; i < board.length; i++) {
+        if (board[i] === 0) emptySpaces++;
+    }
+    if (emptySpaces === 0) {
+        winner = "T";
+        return winner;
+    }
+
+    // check for horizontal
+    for (let i = 0; i <= 6; i +=3) {
+        if (board[i] + board[i + 1] + board[i + 2] === 3) {
+            return 1;
+        } else if (board[i] + board[i + 1] + board[i + 2] === -3) {
+            return -1;
+        }
+    }
+
+    // check for vertical
+    for (let i = 0; i <= 2; i++) {
+        if (board[i] + board[i + 3] + board[i + 6] === 3) {
+            return 1;
+        } else if (board[i] + board[i + 3] + board[i + 6] === -3) {
+            return -1;
+        }
+    }
+
+    // check for diagonals
+    if (board[0] + board[4] + board[8] === 3) {
+        return 1;
+      } else if (board[0] + board[4] + board[8] === -3) {
+        return -1;
+      }else if(board[2] + board[4] + board[6] === 3) {
+        return 1;
+      } else if (board[2] + board[4] + board[6] === -3) {
+        return -1;
+      }
+      return null;
+  }
 
 function render() {
     renderBoard();
     renderMessage();
-    renderMessage();
 }
 
-function renderBoard() {
-    board.forEach(function(colArr, colIdx) {
-      //iterate over the cells and cur column [calArr]
-      colArr.forEach(function(cellVal, rowIdx) {
-        const cellId = `c${colIdx}r${rowIdx}`;
-        const cellEl = document.getElementById(cellId); //cellId gets the div. we can use that to innerText
-        cellEl.style.backgroundColor = COLORS[cellVal]; //cellVal 0 1 -1 LETTERS[cellVal]
-        //shouldn't this work like adding color? 
-        cellEl.innerHTML = LETTERS[cellVal]; 
-      });  
-    });
-  }
-  
+function renderBoard() { 
+  //iterate over the cells
+  board.forEach(function (cellVal, boardArr) {
+    const cellId = `${boardArr}`;
+    const cellEl = document.getElementById(cellId);
+    cellEl.style.backgroundColor = COLORS[cellVal];
+    cellEl.innerHTML = LETTERS[cellVal];
+  });
+}
+
+
   function renderMessage() {
-  if (winner === 'T') { 
-    messageEl.innerText = "You have tied.";
-  } else if (winner) { 
-    messageEl.innerHTML = `<span style="color: ${COLORS[winner]}">${LETTERS[winner]}</span> Wins`; 
+  if (winner === "T") {
+    messageEl.innerText = "It's a Tie!!!";
+    playAgainBtn.style.visibility = "visible";
+  } else if (winner) {
+    messageEl.innerHTML = `<span style="color: ${COLORS[winner]}">${LETTERS[
+      winner
+    ].toUpperCase()}</span> Wins!`;
+    playAgainBtn.style.visibility = "visible";
   } else {
-    messageEl.innerHTML = `<span style="color: ${COLORS[turn]}">${LETTERS[turn]}</span>'s Turn'`; 
+    // Game is in play
+    playAgainBtn.style.visibility = "hidden";
+    messageEl.innerHTML = `<span style="color: ${COLORS[turn]}">${LETTERS[
+      turn
+    ].toUpperCase()}</span>'s Turn`;
   }
-  }
-  
-  function renderControls() {
-   playAgainBtn.style.visibility = winner ? 'visible' : 'hidden';
-  }
-  
-  // squareEls[6] needs to equal board[0][0]
-  //click on <div#c0r0> =  board[0][0] = changes to 1 / -1 = marks x / o = evalautes for win = win || or next move = change player = player clicks the cycle continues till win or tie. 
+}
